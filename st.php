fde66fdbaa3bf7c75a0f1ac43f53a739c09da2c5
abdur-rahman <mdabdurrahman542@gmail.com>
2022-08-08 20:37:18 -0600

    second commt

--- st.php
+++ st.php
@@ -7,18 +7,10 @@
         public function setName($name){
           return  $this->name = $name;
         }
-        public function getName()
-        {
-          return  $this->name;
-        }
     }
  
-    
     $teacher = new Teacher();
-
-    $teacher->setName("Arif");
-    echo $teacher->getName();
-    
+    echo $teacher->setName("Arif");
 
 ?>
        
